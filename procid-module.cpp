12c8bee5de92dfb7b454ca38fc8cde7d39ecb5d3
karloskokskeller <81265103+karloskokskeller@users.noreply.github.com>
2021-03-25 19:03:48 +0100

    Add files via upload

---
+++ procid-module.cpp
@@ -0,0 +1,53 @@
+#include <windows.h>
+#include <TlHelp32.h>
+
+
+uintptr_t GetProcID(const wchar_t* ProcName)
+{
+    DWORD ProcID = 0;
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hSnap != INVALID_HANDLE_VALUE)
+    {
+        PROCESSENTRY32 procEntry;
+        procEntry.dwSize = sizeof(procEntry);
+
+        if (Process32First(hSnap, &procEntry))
+        {
+            do
+            {
+                if (!_wcsicmp(procEntry.szExeFile, ProcName))
+                {
+                    ProcID = procEntry.th32ProcessID;
+                    break;
+
+                }
+            } while (Process32Next(hSnap, &procEntry));
+        }
+    }
+    CloseHandle(hSnap);
+    return ProcID;
+}
+
+uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
+{
+    uintptr_t modBaseAddr = 0;
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
+    if (hSnap != INVALID_HANDLE_VALUE)
+    {
+        MODULEENTRY32 modEntry;
+        modEntry.dwSize = sizeof(modEntry);
+        if (Module32First(hSnap, &modEntry))
+        {
+            do
+            {
+                if (!_wcsicmp(modEntry.szModule, modName))
+                {
+                    modBaseAddr = (uintptr_t)modEntry.modBaseAddr;
+                    break;
+                }
+            } while (Module32Next(hSnap, &modEntry));
+        }
+    }
+    CloseHandle(hSnap);
+    return modBaseAddr;
+}
